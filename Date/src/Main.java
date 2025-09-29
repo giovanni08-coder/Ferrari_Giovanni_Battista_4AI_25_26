7c1362d36836c0f6ca3f1a708eeb2dc9dd5fab11
giovanni08-coder <ferrari.giovannibattist.08@itisdalmine.edu.it>
2025-09-30 00:23:27 +0200

    committo esperimenti su date

---
+++ Date/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    // to see how IntelliJ IDEA suggests fixing it.
+    IO.println(String.format("Hello and welcome!"));
+    Date date= new Date("19/9/2025");
+    System.out.println(date);
+    
+    }
+}
