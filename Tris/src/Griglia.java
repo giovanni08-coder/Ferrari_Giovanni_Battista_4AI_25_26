c7de9f2a5065c9a2522222756a98dc2dd9a0477b
giovanni08-coder <ferrari.giovannibattist.08@itisdalmine.edu.it>
2025-10-14 10:01:29 +0200

    insrisco tris iniziato

---
+++ Tris/src/Griglia.java
@@ -0,0 +1,8 @@
+public class Griglia {
+    private String [][] griglia = new String[3][3];
+    public void Set_Griglia(){
+        for (int i = 0; i < griglia.length; i++)
+        {
+        }
+    }
+}
---
+++ Tris/src/Main.java
@@ -0,0 +1,31 @@
+void main() {
+    Griglia g1 = new Griglia();
+////   int[][] matrix = new int[3][3];
+    int[][] matrix2 = new int[3][3];
+    /*
+    for (int i = 0; i < matrix.length; i++)
+    {
+        // length returns number of rows
+        System.out.print("row " + i + " : ");
+        for (int j = 0; j < matrix[i].length; j++)
+        {
+            // here length returns # of columns corresponding to current row
+            System.out.print("col " + j + "  ");
+        }
+    }*/
+    matrix2[1][0]=1;
+    matrix2[0][0]=5;
+    matrix2[1][2]=8;
+
+   for (int i =0; i<3;i++){
+       for (int j=0;j<3;j++) {
+           System.out.print(matrix2[i][j]);
+           if (j!=2){
+               System.out.print( "|");
+           }
+       }
+       System.out.println();
+       //System.out.println("-|-|-");
+   }
+
+}
