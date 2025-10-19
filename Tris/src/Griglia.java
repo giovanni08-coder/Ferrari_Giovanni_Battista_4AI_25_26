e155a67c0e9415836077c73abbafc69a36633ed5
giovanni08-coder <ferrari.giovannibattist.08@itisdalmine.edu.it>
2025-10-19 02:28:44 +0200

    inserisco il programma non ultimato

--- Tris/src/Griglia.java
+++ Tris/src/Griglia.java
@@ -3,52 +3,87 @@ public class Griglia {
     public final int y = 3;
     public int riga;
     public int colonna;
-    private String grill="";
+    private String grill = "";
     private static char[][] xy;
-    public Griglia(){
-        xy= new char[3][3];
+
+    public Griglia() {
+        xy = new char[3][3];
     }
-    public String Get_griglia(){
-        grill="";
-        for (int i =0; i<3;i++){
-            for (int j=0;j<3;j++) {
-                grill+=xy[i][j];
-                if (j!=2){
-                    grill+="  |";
+
+    public String Get_griglia() {
+        grill = "";
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                grill += xy[i][j];
+                if (j != 2) {
+                    grill += "  |";
                 }
             }
-            grill+="\n";
-            grill+="---|---|---";
-            grill+="\n";
+            grill += "\n";
+            grill += "---|---|---";
+            grill += "\n";
         }
         return grill;
     }
-    public void Set_riga(int riga){
-        if (riga>=x){
+
+    public void Set_riga(int riga) {
+        if (riga >= x) {
             throw new IllegalArgumentException("non puoi inserire il simbolo in questa riga");
         }
-        this.riga=riga;
+        this.riga = riga;
     }
-    public int Get_riga(){
+
+    public int Get_riga() {
         return riga;
     }
-    public void Set_colonna(int colonna){
-        if (colonna>=y){
+
+    public void Set_colonna(int colonna) {
+        if (colonna >= y) {
             throw new IllegalArgumentException("non puoi inserire il simbolo in questa riga");
         }
-        this.colonna=colonna;
+        this.colonna = colonna;
     }
-    public int Get_colonna(){
+
+    public int Get_colonna() {
         return colonna;
     }
-    public String Get_simboli_input1(){
-        xy[Get_riga()][Get_colonna()]='X';
+
+    public String Get_simboli_input1() {
+        if (xy[Get_riga()][Get_colonna()] == 'X' || xy[Get_riga()][Get_colonna()] == '0') {
+            throw new IllegalArgumentException("non puoi sostituire un simbolo");
+        }
+        xy[Get_riga()][Get_colonna()] = 'X';
         return Get_griglia();
 
     }
-    public String Get_simboli_input2(){
-        xy[Get_riga()][Get_colonna()]='0';
+
+    public String Get_simboli_input2() {
+        if (xy[Get_riga()][Get_colonna()] == 'X' || xy[Get_riga()][Get_colonna()] == '0') {
+            throw new IllegalArgumentException("non puoi sostituire un simbolo");
+        }
+        xy[Get_riga()][Get_colonna()] = '0';
         return Get_griglia();
+    }
+    public class Pair<A, B> {
+        public A first;
+        public B second;
+
+        public Pair(A first, B second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
+    public Pair<String,Boolean> Vittoria(Boolean errore){
+        if ((xy[0][0] == 'X' && xy[0][1] == 'X' && xy[0][2] == 'X')||(xy[1][0] == 'X' && xy[1][1] == 'X' && xy[1][2] == 'X')||(xy[2][0] == 'X' && xy[2][1] == 'X' && xy[2][2] == 'X')||(xy[0][1] == 'X' && xy[1][1] == 'X' && xy[2][1] == 'X')||(xy[0][2] == 'X' && xy[1][2] == 'X' && xy[2][2] == 'X')||(xy[0][0] == 'X' && xy[1][0] == 'X' && xy[2][0] == 'X')||(xy[0][0] == 'X' && xy[1][1] == 'X' && xy[2][2] == 'X')||(xy[2][0] == 'X' && xy[1][1] == 'X' && xy[0][2] == 'X')){
+            errore=false;
+            return new Pair<>("Ha vinto il giocatore 1",errore);
+        }
+        if((xy[0][0] == '0' && xy[0][1] == '0' && xy[0][2] == '0')||(xy[1][0] == '0' && xy[1][1] == '0' && xy[1][2] == '0')||(xy[2][0] == '0' && xy[2][1] == '0' && xy[2][2] == '0')||(xy[0][1] == '0' && xy[1][1] == '0' && xy[2][1] == '0')||(xy[0][2] == '0' && xy[1][2] == '0' && xy[2][2] == '0')||(xy[0][0] == '0' && xy[1][0] == '0' && xy[2][0] == '0')||(xy[0][0] == '0' && xy[1][1] == '0' && xy[2][2] == '0')||(xy[2][0] == '0' && xy[1][1] == '0' && xy[0][2] == '0')){
+            errore=false;
+            return new Pair<>("Ha vinto il giocatore 2",errore);
+        }
+        errore=false;
+        return new Pair<>("non ha vinto nessuno",errore);
     }
 }
 
--- Tris/src/Main.java
+++ Tris/src/Main.java
@@ -1,7 +1,7 @@
 void main() {
     Griglia g1 = new Griglia();
 ////   int[][] matrix = new int[3][3];
-    int[][] matrix2 = new int[3][3];
+    char[][] matrix2 = new char[3][3];
     /*
     for (int i = 0; i < matrix.length; i++)
     {
@@ -30,18 +30,22 @@ void main() {
    }
     */
     System.out.println(g1.Get_griglia());
-    String s1=IO.readln("Giocatore 1 inserisci la riga in cui mettere il tuo simbolo: ");
-    int riga= Integer.parseInt(s1);
-    g1.Set_riga(riga);
-    String s2=IO.readln("Giocatore 1 inserisci la riga in cui mettere il tuo simbolo: ");
-    int colonna= Integer.parseInt(s2);
-    g1.Set_colonna(colonna);
-    System.out.println(g1.Get_simboli_input1());
-    String s3=IO.readln("Giocatore 1 inserisci la riga in cui mettere il tuo simbolo: ");
-    int rig= Integer.parseInt(s3);
-    g1.Set_riga(rig);
-    String s4=IO.readln("Giocatore 1 inserisci la riga in cui mettere il tuo simbolo: ");
-    int colonn= Integer.parseInt(s4);
-    g1.Set_colonna(colonn);
-    System.out.println(g1.Get_simboli_input2());
+    Boolean errore=true;
+    while (errore) {
+        String s1 = IO.readln("Giocatore 1 inserisci la riga in cui mettere il tuo simbolo: ");
+        int riga = Integer.parseInt(s1);
+        g1.Set_riga(riga);
+        String s2 = IO.readln("Giocatore 1 inserisci la colonna in cui mettere il tuo simbolo: ");
+        int colonna = Integer.parseInt(s2);
+        g1.Set_colonna(colonna);
+        System.out.println(g1.Get_simboli_input1());
+        String s3 = IO.readln("Giocatore 1 inserisci la riga in cui mettere il tuo simbolo: ");
+        int rig = Integer.parseInt(s3);
+        g1.Set_riga(rig);
+        String s4 = IO.readln("Giocatore 1 inserisci la colonna in cui mettere il tuo simbolo: ");
+        int colonn = Integer.parseInt(s4);
+        g1.Set_colonna(colonn);
+        System.out.println(g1.Get_simboli_input2());
+        g1.Vittoria(errore);
+    }
 }
