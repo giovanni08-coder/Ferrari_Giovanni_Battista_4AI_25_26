3cb40c6ae4cdf03e73d6b4c2e7dd19f7796e2425
GIOVANNI BATTISTA FERRARI <ferrari.giovannibattist.08@itisdalmine.edu.it>
2025-10-22 13:57:05 +0200

    inserisco il forza 4 quasi finito

--- Forza 4/src/Forza4.java
+++ Forza 4/src/Forza4.java
@@ -1,53 +1,77 @@
 public class Forza4 {
     public final int x = 6;
     public final int y = 7;
-    public int riga;
     public String colonna;
     private char[][] griglia;
     private String grill;
-    public Forza4(char[][] matrice) {
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < y; j++) {
-                matrice[i][j]=' ';
-            }
-        }
-        griglia=matrice;
-    }
-    public void Set_griglia(char[][] matrice,String grill) {
-        this.grill=grill;
-        griglia=matrice;
+    private char simbolo_g1='O';
+    private char simbolo_g2='X';
+    private char simbolo_gioc_corrente;
+    public Forza4() {
         for (int i = 0; i < x; i++) {
             for (int j = 0; j < y; j++) {
-                this.grill += griglia[i][j];
+                griglia[i][j]=' ';
+            }
+        }
+        simbolo_gioc_corrente = simbolo_g1;
+    }
+    public void Set_griglia() {
+        grill="";
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                grill += griglia[i][j];
                 if (j!=6) {
                     this.grill += "  |";
                 }
             }
-            this.grill += "\n";
-            this.grill += "---|---|---|---|---|---|---";
-            this.grill += "\n";
+            grill += "\n";
+            grill += "---|---|---|---|---|---|---";
+            grill += "\n";
         }
     }
     public String Get_griglia(){
         return grill;
     }
+
+
+
     public void Set_verifica_string(String s1){
         if (s1=="" || !s1.matches("[1-7]")){
             throw new IllegalArgumentException("non puoi inserire il seguente valore ");
         }
-        colonna=s1;
     }
+
     public String Get_colonna(){
         return colonna;
     }
-    public void Set_inserisci(int colonna){
-        if (riga>x){
-            throw new IllegalArgumentException("non puoi inserire gettoni in questa colonna perchè è piena");
-        }
-        
+
+    public boolean Verifica_vittoria(){
+        return false;
     }
 
+    public void Inserisci(int colonna){
+        colonna-=1;
+        if(colonna<0 || colonna>6){
+            throw new IllegalArgumentException("ma che caccio di colonna hai chiesto???");
+        }
+        if(griglia[0][colonna] != ' '){
+            throw new IllegalArgumentException("ueeee.... ma non hai visto che la colonna è piena!!!");
+        }
 
+        int fondo = 5;
+        while(griglia[fondo][colonna] != ' '){
+            fondo--;
+        }
+        griglia[fondo][colonna] = simbolo_gioc_corrente;
 
-
+        if(Verifica_vittoria() == false) {
+            if (simbolo_gioc_corrente == simbolo_g1) simbolo_gioc_corrente = simbolo_g2;
+            else simbolo_gioc_corrente = simbolo_g1;
+        }
+    }
 }
+
+
+
+
+
--- Forza 4/src/Main.java
+++ Forza 4/src/Main.java
@@ -1,14 +1,22 @@
 void main() {
-    char[][] griglia=new char[6][7];
-    String grill="";
-    Forza4 f1=new Forza4(griglia);
-    f1.Set_griglia(griglia,grill);
+    Forza4 f1=new Forza4();
+    f1.Set_griglia();
     System.out.println(f1.Get_griglia());
     System.out.println("Giocatore 1 ");
     String s1 = IO.readln("inserisci il numero della colonna in cui vuoi inserire il gettone: ");
     f1.Set_verifica_string(s1);
     int colonna=Integer.parseInt(f1.Get_colonna());
     colonna-=1;
+    f1.Inserisci(colonna);
+    System.out.println(f1.Get_griglia());
+    System.out.println("Giocatore 1 ");
+    String s2 = IO.readln("inserisci il numero della colonna in cui vuoi inserire il gettone: ");
+    f1.Set_verifica_string(s2);
+    int colonn=Integer.parseInt(f1.Get_colonna());
+    colonn-=1;
+    f1.Inserisci(colonn);
+    System.out.println(f1.Get_griglia());
+
 
 
 }
