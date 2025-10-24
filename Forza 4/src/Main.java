ade15de0221fa90052220c70a407aa493b7384a9
giovanni08-coder <ferrari.giovannibattist.08@itisdalmine.edu.it>
2025-10-24 20:38:43 +0200

    inserisco il forza 4 completato

--- Forza 4/src/Main.java
+++ Forza 4/src/Main.java
@@ -5,25 +5,29 @@ void main() {
     System.out.println("Giocatore 1 ");
     boolean errore=true;
     while (errore) {
-        String s1 = IO.readln("inserisci il numero della colonna in cui vuoi inserire il gettone: ");
-        if (s1.equals("") || !s1.matches("[1-7]")) {
-            throw new IllegalArgumentException("non puoi inserire il seguente valore ");
+        while (errore) {
+            String s1 = IO.readln("inserisci il numero della colonna in cui vuoi inserire il gettone: ");
+            if (s1.equals("") || !s1.matches("[1-7]")) {
+                throw new IllegalArgumentException("non puoi inserire il seguente valore ");
+            }
+            int colonna = Integer.parseInt(s1);
+            f1.Inserisci(colonna);
+            f1.Set_griglia();
+            System.out.println(f1.Get_griglia());
+            errore=f1.Verifica_vittoria();
         }
-        int colonna = Integer.parseInt(s1);
-        f1.Inserisci(colonna);
-        f1.Set_griglia();
-        System.out.println(f1.Get_griglia());
-        errore=f1.Verifica_vittoria();
-        System.out.println("Giocatore 2 ");
-        String s2 = IO.readln("inserisci il numero della colonna in cui vuoi inserire il gettone: ");
-        if (s2=="" || !s2.matches("[1-7]")) {
-            throw new IllegalArgumentException("non puoi inserire il seguente valore ");
+        while (errore) {
+            System.out.println("Giocatore 2 ");
+            String s2 = IO.readln("inserisci il numero della colonna in cui vuoi inserire il gettone: ");
+            if (s2 == "" || !s2.matches("[1-7]")) {
+                throw new IllegalArgumentException("non puoi inserire il seguente valore ");
+            }
+            int colonn = Integer.parseInt(s2);
+            f1.Inserisci(colonn);
+            f1.Set_griglia();
+            System.out.println(f1.Get_griglia());
+            errore = f1.Verifica_vittoria();
         }
-        int colonn = Integer.parseInt(s2);
-        f1.Inserisci(colonn);
-        f1.Set_griglia();
-        System.out.println(f1.Get_griglia());
-        errore=f1.Verifica_vittoria();
     }
 
 
