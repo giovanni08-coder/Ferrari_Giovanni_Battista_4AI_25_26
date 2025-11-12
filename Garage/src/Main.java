67fe8725543abaca9e3cb3003aef2c29fd436d00
GIOVANNI BATTISTA FERRARI <ferrari.giovannibattist.08@itisdalmine.edu.it>
2025-11-12 13:56:46 +0100

    inserisco il cruda da finire

--- Garage/src/Main.java
+++ Garage/src/Main.java
@@ -1,4 +1,87 @@
+List<Scouterino> parcheggio= new ArrayList<Scouterino>();
+
+void datiDiProva() {
+    //parcheggio.add(new Scouterino("Malaguti Phantom F10", "A13CDE", 10000, LocalDate.of(2000, 1, 29)));
+    parcheggio.add(new Scouterino("KTM ", "ADFE23", 10000, LocalDate.of(2005, 1, 29)));
+    //parcheggio.add(new Scouterino("Vespa primavera", "L123D3", 10000, LocalDate.of(2010, 1, 29)));
+    //parcheggio.add(new Scouterino("Aprilia 125", "QJE123", 10000, LocalDate.of(2020, 1, 29)));
+}
+
+void visualizza_tutti(){
+
+    if (parcheggio.size()>0) {
+        IO.println("In archivio sono presenti " + parcheggio.size() + " scuoter");
+        for (Scouterino scouter : parcheggio) {
+            IO.println(scouter);
+        }
+    }
+    else {
+        IO.println("Non sono presenti elementi");
+    }
+}
+void Menu(){
+    System.out.println("Premi 0 per uscire");
+    System.out.println("Premi 1 per aggiungere");
+    System.out.println("Premi 2 per eliminare");
+    System.out.println("Premi 3 per visualizzare");
+    System.out.println("Premi 4 per modifica");
+}
+void Aggiunta(){
+    try {
+        String t = IO.readln("inserisci targa");
+        int k = Integer.parseInt(IO.readln("Inserisci i km"));
+        String m = IO.readln("inserisci il modello");
+        LocalDate d = LocalDate.parse(IO.readln("Inserisci la data"));
+        Scouterino s1 = new Scouterino(m,t,k,d);
+        parcheggio.add(s1);
+    } catch (Exception e) {
+        IO.println(e.getMessage());
+    }
+}
+
+void Rimuovi(){
+    String t = IO.readln("inserisci targa dello scuoter da cancellare");
+    Scouterino s= null;
+    int i =0;
+    while (s ==null && i<parcheggio.size()) {
+
+        if (parcheggio.get(i).getTarga().equals(t)) {
+            s = parcheggio.get(i);
+        }
+        i++;
+    }
+    parcheggio.remove(s);
+}
+void Modifica(){
+    String t = IO.readln("inserisci targa dello scuoter da cancellare");
+    Scouterino s= null;
+    int i =0;
+    while (s ==null && i<parcheggio.size()) {
+
+        if (parcheggio.get(i).getTarga().equals(t)) {
+            s = parcheggio.get(i);
+        }
+        i++;
+    }
+    parcheggio.re;
+}
+
+
 void main() {
-    Scouterino s1=new Scouterino("a","b",1,2);
-    s1.setTarga("12Ab56");
+    datiDiProva();
+    int scelta = 0;
+    do {
+        Menu();
+        scelta= Integer.parseInt(IO.readln("Inseisci cosa vuoi fare"));
+        switch (scelta){
+            case 1: Aggiunta();break;
+            case 2: Rimuovi();break;
+            case 3: visualizza_tutti();break;
+            case 4: Modifica();break;
+            case 0:break;
+            default:
+                System.out.println("Sei uno stupidino non puoi inserire altri numeri");
+
+        }
+    }while (scelta!=0);
 }
--- Garage/src/Scouterino.java
+++ Garage/src/Scouterino.java
@@ -1,16 +1,21 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 public class Scouterino {
+    static private List<String> targhe = new ArrayList<String>();
     private String modello;
     private String targa;
     private int chilometri_fatti;
     private LocalDate dataAcquisto;
 
-    public Scouterino(String m, String t,int c,int d){
-        modello =  getTarga();
-        targa = "XEBC12";
+    public Scouterino(String m, String t,int c,LocalDate d){
+        Set_modello(m);
+        setTarga(t);
+        setChilometri_fatti(c);
+        setDataAcquisto(d);
     }
     public String getTarga(){
         return targa;
@@ -18,26 +23,61 @@ public class Scouterino {
     //LJ3663
     // 6 caratteri alfanumerici primo carattere deve essere una lettera
     public void setTarga(String t ){
-            if (!t.substring(0,1).matches("[A-Z]") && !t.substring(1).matches("[A-Z][0-9]")) {
-                throw new IllegalArgumentException("Non puoi mettere il primo carattere nè minuscolo nè un numero");
+            if(t.matches("[A-Z][0-9A-Z]{5}")==false){
+                throw new IllegalArgumentException("la targa è sbagliata");
             }
+            if (targhe.contains(t)){
+                throw new IllegalArgumentException("Targa già usata");
+            }
+            targhe.add(t);
             targa =t;
+            /* soluzione mia
+        if (t == null || t.length() < 7) {
+            throw new IllegalArgumentException("La targa deve avere almeno 6 caratteri");
+        }
+        if (!t.substring(0,1).matches("[A-Z]")){
+            throw new IllegalArgumentException("la prima lettera deve essere maiuscola");
+        }
+        if (!t.substring(1).matches("[A-Z][0-9]+")) {
+            throw new IllegalArgumentException("Non puoi mettere il primo carattere nè minuscolo nè un numero");
+        }*/
     }
     public String getModello(){
         return modello;
     }
-    public void Set_modello() {
+    public void Set_modello(String m) {
+        if (m.length()<2){
+            throw new IllegalArgumentException("Descrizione del computer troppo corta");
+        }
+        modello =m;
     }
     public int getChilometri_fatti(){
         return chilometri_fatti;
     }
-    public void setChilometri_fatti(){
-
+    public void setChilometri_fatti(int k){
+        if (chilometri_fatti<0){
+            throw new IllegalArgumentException("Kilometraggio negativo noon consetito");
+        }
+        chilometri_fatti=k;
     }
     public LocalDate getData_acquisto(){
         return dataAcquisto;
     }
 
     public void setDataAcquisto(LocalDate d) {
+        if (d.isAfter(LocalDate.now())){
+            throw new IllegalArgumentException("Data non usabile");
+        }
+        dataAcquisto = d;
+    }
+
+    @Override
+    public String toString() {
+        return "Scouterino{" +
+                "modello='" + modello + '\'' +
+                ", targa='" + targa + '\'' +
+                ", chilometri_fatti=" + chilometri_fatti +
+                ", dataAcquisto=" + dataAcquisto +
+                '}';
     }
 }
