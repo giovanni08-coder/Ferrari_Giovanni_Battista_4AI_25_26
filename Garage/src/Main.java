96c2584f2ee9df6cb4ba7171be3d30ce67a32462
GIOVANNI BATTISTA FERRARI <ferrari.giovannibattist.08@itisdalmine.edu.it>
2025-11-18 09:09:48 +0100

    Lavoro lab quasi completato

--- Garage/src/Main.java
+++ Garage/src/Main.java
@@ -1,10 +1,12 @@
 List<Scouterino> parcheggio= new ArrayList<Scouterino>();
-
+// implementare: se targa è presente in archivio e mostra i dati,targa parziale prime 3 cifre,chilometraggio un range di chilometri km minimo e km massimo,ordinamento in base a filtro scelta, ricerca in base al modello,ricerca in base alla data
 void datiDiProva() {
     //parcheggio.add(new Scouterino("Malaguti Phantom F10", "A13CDE", 10000, LocalDate.of(2000, 1, 29)));
-    parcheggio.add(new Scouterino("KTM ", "ADFE23", 10000, LocalDate.of(2005, 1, 29)));
+    parcheggio.add(new Scouterino("KTM ", "ADFE23", 500, LocalDate.of(2005, 1, 29)));
     //parcheggio.add(new Scouterino("Vespa primavera", "L123D3", 10000, LocalDate.of(2010, 1, 29)));
     //parcheggio.add(new Scouterino("Aprilia 125", "QJE123", 10000, LocalDate.of(2020, 1, 29)));
+    parcheggio.add(new Scouterino("KTM ", "ADFQ24", 8000, LocalDate.of(2005, 1, 29)));
+    parcheggio.add(new Scouterino("KTM ", "QCDL27", 15000, LocalDate.of(2005, 1, 29)));
 }
 
 void visualizza_tutti(){
@@ -25,6 +27,78 @@ void Menu(){
     System.out.println("Premi 2 per eliminare");
     System.out.println("Premi 3 per visualizzare");
     System.out.println("Premi 4 per modifica");
+}
+void Targa_presente(){
+    String t = IO.readln("inserisci targa dello scuoter");
+    Scouterino s= null;
+    int i =0;
+    while (s == null && i<parcheggio.size()) {
+
+        if (parcheggio.get(i).getTarga().equals(t)) {
+            System.out.println("La targa è presente");
+            System.out.println("La targa è: " + t );
+            s = parcheggio.get(i);
+        }
+        i++;
+    }
+    if (s==null) {
+        System.out.println("La targa: " + t + " non è presente");
+    }
+    else System.out.println(s);
+}
+void Targa_parziale(){
+    String t = IO.readln("inserisci targa dello parziale scuoter");
+    List<Scouterino> lista = new ArrayList<>();
+    Scouterino s= null;
+    int i =0;
+    while (i<parcheggio.size()) {
+
+        if (parcheggio.get(i).getTarga().contains(t)) {
+            s = parcheggio.get(i);
+            lista.add(s);
+        }
+        i++;
+    }
+    if (s==null){
+        System.out.println("Data non presente");
+    }
+    for (int elementi = 0;elementi<lista.size();elementi++){
+        System.out.println(lista.get(elementi));
+    }
+}
+void Ricercakm(){
+    final int Min_km = Integer.parseInt(IO.readln("inserisci il minimo di chilometri utilizzabili: "));
+    final int Max_km = Integer.parseInt(IO.readln("inserisci il massimo di chilometri utilizzabili: "));
+    List<Scouterino> l = new ArrayList<>();
+    Scouterino s= null;
+    int i =0;
+    while (i<parcheggio.size()) {
+
+        if (parcheggio.get(i).getChilometri_fatti()>=Min_km && parcheggio.get(i).getChilometri_fatti()<=Max_km) {
+            s = parcheggio.get(i);
+            l.add(s);
+        }
+        i++;
+    }
+    if (s==null){
+        System.out.println("Data non presente");
+    }
+    for (int elementi = 0;elementi<l.size();elementi++){
+        System.out.println(l.get(elementi));
+    }
+}
+void OrdinaperChilometri(){
+    Map<Integer,Scouterino> dizionario = new HashMap<>();
+    Scouterino s= null;
+    int i =0;
+    while (i<parcheggio.size()) {
+        s = parcheggio.get(i);
+        dizionario.put(parcheggio.get(i).getChilometri_fatti(), s);
+        i++;
+    }
+    for (int ordina = 0; ordina<dizionario.size(); ordina++){
+        if (dizionario.get(dizionario.).keySet()>dizionario.keySet());
+    }
 }
 void Aggiunta(){
     try {
@@ -63,12 +137,13 @@ void Modifica(){
         }
         i++;
     }
-    parcheggio.re;
 }
 
 
 void main() {
+
     datiDiProva();
+    /*
     int scelta = 0;
     do {
         Menu();
@@ -83,5 +158,7 @@ void main() {
                 System.out.println("Sei uno stupidino non puoi inserire altri numeri");
 
         }
-    }while (scelta!=0);
+    }while (scelta!=0);*/
+    System.out.println(parcheggio);
+    Ricercakm();
 }
