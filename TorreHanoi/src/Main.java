e70a75f9b1e28e886b040fdb1de4023f064d37cf
giovanni08-coder <ferrari.giovannibattist.08@itisdalmine.edu.it>
2025-11-23 11:30:39 +0100

    inserisco gioco della torre di Hanoi

--- TorreHanoi/src/Main.java
+++ TorreHanoi/src/Main.java
@@ -1,15 +1,33 @@
+void InFirstRastrelliera(Rastrelliera r ){
+    for (int i = r.numeroDischiPossibili; i >= 1; i--){
+        r.InserisciDisco(r,new Disco(i));
+    }
+}
 void main() {
+    boolean errore = false;
     Stack<Integer> stack = new Stack<Integer>();
     System.out.println(stack);
     Rastrelliera r = new Rastrelliera(7,"A");
-    Stack<Integer> t = new Stack<>();
-    r.InserisciDisco(r,new Disco(1));
-    r.InserisciDisco(r,new Disco(2));
-    r.InserisciDisco(r,new Disco(3));
-    r.InserisciDisco(r,new Disco(4));
-    r.InserisciDisco(r,new Disco(5));
-    r.InserisciDisco(r,new Disco(6));
-    r.InserisciDisco(r,new Disco(7));
-    System.out.println(r.RastrellieraCompleta(r));
-
+    Rastrelliera r2 = new Rastrelliera(7,"B");
+    Rastrelliera r3 = new Rastrelliera(7,"C");
+    Map<String,Rastrelliera> rastrellieraMap = new HashMap<>();
+    rastrellieraMap.put(r.getNome(),r);
+    rastrellieraMap.put(r2.getNome(),r2);
+    rastrellieraMap.put(r3.getNome(),r3);
+    InFirstRastrelliera(r);
+    while (errore == false) {
+        System.out.println(r.toString());
+        System.out.println(r2.toString());
+        System.out.println(r3.toString());
+        try {
+            String mittente = IO.readln("Inserisci la rastrelliera mittente");
+            String destinatario = IO.readln("Inserisci la rastrelliera destinataria");
+            r.spostaDisco(rastrellieraMap.get(mittente), rastrellieraMap.get(destinatario));
+            errore = r.RastrellieraCompleta(r3);
+        } catch (Exception e) {
+            System.out.println("riprova");
+            errore = false;
+        }
+    }
+    System.out.println("Hai vinto");
 }
--- TorreHanoi/src/Rastrelliera.java
+++ TorreHanoi/src/Rastrelliera.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 public class Rastrelliera {
     private String nome;
     private Stack<Disco> stack = new Stack<Disco>();
-    private final int numeroDischiPossibili;
+    public final int numeroDischiPossibili;
     public Rastrelliera(final int numeroDischiPossibili,String nome){
         this.numeroDischiPossibili = numeroDischiPossibili;
         setNome(nome);
@@ -23,16 +23,30 @@ public class Rastrelliera {
     }
 
     public void InserisciDisco(Rastrelliera rastrelliera,Disco disco){
-        if (rastrelliera.stack.size()!=0 && disco.getDimensione() < rastrelliera.stack.getFirst().getDimensione()){
+        if (rastrelliera.stack.size()!=0 && disco.getDimensione() > rastrelliera.stack.getFirst().getDimensione()){
             throw new IllegalArgumentException("Non puoi inserire questo disco");
         }
         rastrelliera.stack.add(disco);
     }
 
     public void spostaDisco(Rastrelliera mittente, Rastrelliera ricevente){
+        if (mittente == null ){
+            throw new IllegalArgumentException("Non puoi togliere qualcosa che non c'è");
+        }
             InserisciDisco(ricevente,mittente.stack.get(0));
             mittente.stack.pop();
     }
-    public String RastrellieraCompleta(Rastrelliera rastrelliera){
+    public boolean RastrellieraCompleta(Rastrelliera rastrelliera){
+        if (rastrelliera.stack.size() != numeroDischiPossibili){
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Rastrelliera{" +
+                "stack=" + stack +
+                '}';
     }
 }
