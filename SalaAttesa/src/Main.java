19f7bb183d76f0a93f8d0d088e2476287c8ec25d
giovanni08-coder <ferrari.giovannibattist.08@itisdalmine.edu.it>
2026-01-04 16:15:28 +0100

    inserisco il codice finito, Manca solo il problema del log

--- SalaAttesa/src/Main.java
+++ SalaAttesa/src/Main.java
@@ -1,41 +1,72 @@
 import java.util.Scanner;
 import java.sql.Timestamp;
 
-        private Cassa[] casse;
-        private Gestore gestore;
-        private LogChiamate log;
 
-         void main() {
-            log = new LogChiamate();
-            gestore = new Gestore(log);
-            casse = new Cassa[] { new Cassa() };
-            int scelta;
+    private Cassa[] casse;
+    private Gestore gestore;
+    private LogChiamate log;
 
-            do {
-                stampaMenu();
-                scelta = Integer.parseInt(IO.readln("Inserisce un numero da 0 a 5: "));
-                switch (scelta) {
-                    case 1 : gestore.emettiBigliettoNormale();break;
-                    case 2 : gestore.emettiBigliettoPtioritario();break;
-                    case 3 : {
-                        for (Cassa c : casse) {
-                            String gest = gestore.chiamaBiglietto();
-                            String ind = c.getIdentificativo();
-                            System.out.println(gest + ":" + c.ChiamaProssimoCliente());
-                            log.AggiungiChiamate(gest,ind,new Timestamp(System.currentTimeMillis()));
-                        }
-                    };break;
-                    case 4 : System.out.println("In attesa: " + gestore.getBigliettiInAttesa());break;
-                    case 5 :
-                        for (int i =0; i<log.getChiamate().size();i++) {
-                        System.out.println(log.getChiamate().get(i));
-                    }break;
-                }
-            } while (scelta != 0);
+    public void prenotaBigliettoNormale() {
+        gestore.emettiBigliettoNormale();
+    }
+    public void prenotaBigliettoPrioritario() {
+        gestore.emettiBigliettoPtioritario();
+    }
+    public void StampaUtentiInCoda(){
+        System.out.println("In attesa: " + gestore.getBigliettiInAttesa());
+    }
+    public void StampaStoricoChiamate(){
+        for (int i =0; i<log.getChiamate().size();i++) {
+            System.out.println(log.getChiamate().get(i));
         }
+    }
+    private int ChiediScelta(){
+        return Integer.parseInt(IO.readln("Inserisci un numero da 0 a 5: "));
+    }
+    void main() {
+        log = new LogChiamate();
+        gestore = new Gestore(log);
+        casse = new Cassa[]{ new Cassa() };
 
-        public static void stampaMenu() {
-            System.out.println("""
+        int scelta;
+
+        do {
+            stampaMenu();
+            scelta = ChiediScelta();
+
+            switch (scelta) {
+                case 1:
+                    prenotaBigliettoNormale();
+                    break;
+
+                case 2:
+                    prenotaBigliettoPrioritario();
+                    break;
+
+                case 3:
+                    for (Cassa c : casse) {
+                        String gest = gestore.chiamaBiglietto();
+                        String ind = c.getIdentificativo();
+                        System.out.println(gest + ":" + c.ChiamaProssimoCliente());
+                        log.AggiungiChiamate(gest, ind, new Timestamp(System.currentTimeMillis()));
+                    }
+                    break;
+
+                case 4:
+                    StampaUtentiInCoda();
+                    break;
+
+                case 5:
+                    StampaStoricoChiamate();
+                    break;
+            }
+
+        } while (scelta != 0);
+    }
+
+
+    public void stampaMenu() {
+        System.out.println("""
                 1. Prenota biglietto normale
                 2. Prenota biglietto prioritario
                 3. Chiamata pazienti
@@ -43,5 +74,5 @@ import java.sql.Timestamp;
                 5. Storico chiamate
                 0. Esci
                 """);
-        }
+    }
 
